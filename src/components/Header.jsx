511668c6fcbd4b1ac8db18a3f008a45461272837
coolkheng <122100699+coolkheng@users.noreply.github.com>
2024-04-25 18:00:59 +0800

    installed some library

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import "../style/Header.css"
-import DropdownMenu from './UsernameDropDown';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "../style/Header.css";
+import DropdownMenu from "./UsernameDropDown";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = ({ children }) => {
-  const [menu,setMenu] = useState("Home");
+  const [menu, setMenu] = useState("Home");
   return (
     <div className="header">
       <ul className="nav_menu">
-                <li onClick={()=>{setMenu("Home")}}>Home{menu==="Home"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("Product From Suppliers")}}><Link to="/suppliers" style={{textDecoration:'none'}}>Products From Suppliers</Link>{menu==="Product From Suppliers"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("Your Store")}}><Link to="/products" style={{textDecoration: 'none'}}>Your Store</Link>{menu==="Your Store"?<hr/>:<></>}</li>
-               
-                
-                <div className="dropdown">
-        <DropdownMenu />
-      </div>
-                
-            </ul>
-          
+        <div className="dropdown">
+          <DropdownMenu />
+        </div>
+      </ul>
     </div>
   );
 };
