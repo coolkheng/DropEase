6cb7aa2cb59d2ac6ff564098090376c5770c407d
rachelfong0320 <rachelfong.tw@gmail.com>
2024-06-09 13:01:01 +0800

    Cart (Rachel Fong)

--- src/pages/Cart(Customer).jsx
+++ src/pages/Cart(Customer).jsx
@@ -1,15 +1,11 @@
 import React, { useContext, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import { FaXmark } from "react-icons/fa6";
 import { FaPlus, FaMinus } from "react-icons/fa";
 import HeaderCustomer from "../components/Header(Customer)";
 import { CartContext } from "./cartContext";
 import "../style/Cart(Customer).css";
-import { loadStripe } from "@stripe/stripe-js";
-import axios from "axios";
 
 const Cart = () => {
-  let Id = useParams();
   const { cartItems, addToCart, decreaseQty, removeFromCart } = useContext(CartContext);
   const authToken = localStorage.getItem('auth-token');
 
@@ -19,122 +15,10 @@ const Cart = () => {
     0
   );
 
-  const handleCheckout = async () => {
-    console.log("Handle checkout function called!");
-
-    await clearCart();
-
-    const stripe = await loadStripe(
-      "pk_test_51PNRN72MhvOMkL1SuBf1xlugNRrOIaWjFrNyg80sHZbgkCSwHrf50jA6oHUq04d03PaVvYlL9aZ9GAlC4i7IhtT400byNPNV9D"
-    );
-
-    const body = {
-      products: cartItems.map((item) => ({
-        name: item.name,
-        price: item.price,
-        quantity: item.qty,
-        image: item.mainImages,
-      })),
-      userId: localStorage.getItem("auth-token"),
-    };
-
-    const userId = localStorage.getItem("auth-token");
-
-    const headers = {
-      "Content-Type": "application/json",
-      "auth-token": localStorage.getItem("auth-token"),
-    };
-
-    const productsData = cartItems.map((item) => ({
-      productName: item.name,
-      price: item.price,
-      quantity: item.qty,
-      productImage: item.mainImages,
-      category: item.category || "Uncategorized",
-    }));
-
-    const orderData = {
-      products: productsData,
-      customer: {
-        name: "Customer Name", // Replace with actual customer data
-        email: "customer@example.com", // Replace with actual customer data
-      },
-      orderDate: new Date(),
-      payment_status: "Paid", // You may adjust this based on your business logic
-      delivery_status: "Ready to Ship", // You may adjust this based on your business logic
-    };
-
-    // Make an HTTP request to your backend API to create the order
-    try {
-      const orderResponse = await axios.post(
-        "http://localhost:4000/insert_orders",
-        orderData,
-        { headers }
-      );
-
-      console.log("Order response from backend:", orderResponse.data);
-    } catch (error) {
-      console.error("Checkout error:", error);
-    }
-
-    try {
-      const response = await axios.post(
-        "http://localhost:4000/create-checkout-session-customer",
-        body,
-        { headers }
-      );
-
-      console.log("Response from backend:", response);
-
-      if (response.status !== 200) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const { id: sessionId } = response.data;
-
-      const result = await stripe.redirectToCheckout({
-        sessionId: sessionId,
-      });
-
-      if (result.error) {
-        console.log(result.error.message);
-      } else {
-        console.log("Redirect to checkout successful");
-      }
-    } catch (error) {
-      console.error("Checkout error:", error);
-    }
-  };
-
-  const clearCart = async () => {
-    console.log("Clear cart function called!");
-    try {
-      const token = localStorage.getItem("auth-token");
-      const response = await axios.post(
-        "http://localhost:4000/clear",
-        {},
-        {
-          headers: {
-            "auth-token": token,
-          },
-        }
-      );
-
-      if (response.data.success) {
-        console.log("Cart cleared successfully");
-      } else {
-        console.log("Failed to clear cart");
-      }
-    } catch (error) {
-      console.error("Error clearing cart:", error);
-    }
-  };
-
-
   // Render cart items
   return (
     <>
-      <HeaderCustomer customer={Id}/>
+      <HeaderCustomer />
       <section className="cart-items">
         <div className="container-cart">
           <div className="cart-details">
@@ -195,7 +79,7 @@ const Cart = () => {
                 <h4>Total Price :</h4>
                 <h3>RM {totalPrice.toFixed(2)}</h3>
               </div>
-              <button onClick={handleCheckout} className="checkout-button">Proceed to Checkout</button>
+              <button className="checkout-button">Proceed to Checkout</button>
             </div>
           </div>
         </div>
